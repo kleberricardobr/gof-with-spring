5863d8fad4ab0fbba70bc56827e73e43627b9466
Kleber <kleber_ricardobr@yahoo.com.br>
2025-04-25 10:06:01 -0300

    ajustes no carregamento de informações

---
+++ src/main/java/com/estudos/gof/dto/ClienteResponseDTO.java
@@ -0,0 +1,25 @@
+package com.estudos.gof.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ClienteResponseDTO {
+    @NotBlank(message = "{campo.notblank}")
+    @Size(max = 500)
+    private String nome;
+
+    @NotBlank(message = "{campo.notblank}")
+    @Size(max = 150, message = "{campo.maxsize}")
+    private String telefone;
+
+    @NotBlank(message = "{campo.notblank}")
+    @Size(max = 15, message = "{campo.maxsize}")
+    private String cpf;
+
+    @NotBlank(message = "{campo.notblank}")
+    private String cep;
+}
